45741f95902da7c3226ae6635a3d6617f6c66b6c
Aashish Adhikari <aashish@pop-os.localdomain>
2023-02-13 09:22:33 +0545

    Removed ignore files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def index(request):             
-    message = "Hello, world. Your at the polls index"
-    return HttpResponse(message) #type: ignore
+    return HttpResponse('Hello, world. Your at the polls index')
+ 
